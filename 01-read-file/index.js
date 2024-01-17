16546baf2140fe851c6f340a1d7e3e8105b88e44
VictorREDrudko <viktor_RED_rudko@mail.ru>
2024-01-17 21:12:13 +0300

    feat: read file

---
+++ 01-read-file/index.js
@@ -0,0 +1,20 @@
+// Подключаем необходимые модули
+// 1.МОДУЛЬ ФАЙЛОВОЙ СИСТЕМЫ:
+const fs = require('fs');
+// 2.МОДУЛЬ ДЛЯ РАБОТЫ С ПУТЯМИ:
+const path = require('path');
+
+// Путь к файлу, который нужно прочитать:
+const pathToFile = './text.txt'
+
+// Вывод содержимого файла text.txt в консоль (метод fs.readFile):
+// ВАРИАНТ 1:
+// путь к файлу + колбэк функция (error - содержит сведения о проблемах, если чтение файла не удалось)
+fs.readFile(pathToFile, (error, data) => {
+  console.log(data.toString());
+});
+
+// ВАРИАНТ 2 (с указанием кодировки utf8):
+// fs.readFile(pathToFile, 'utf8', (error, data) => {
+//   console.log(data);
+// });

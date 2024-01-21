c81aa28005799f5713e64992059872fd37551f57
VictorREDrudko <viktor_RED_rudko@mail.ru>
2024-01-21 16:33:44 +0300

    refactor: add new comments (04)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -8,6 +8,7 @@ const pathToFolderCopy = './04-copy-directory/files-copy';
 
 // Создадим асинхронную функцию:
 async function copyDir() {
+  // Создадим папку files-copy, если она еще не существует
   try {
     await fsPromises.mkdir(pathToFolderCopy, {
       recursive: true,
@@ -15,7 +16,7 @@ async function copyDir() {
   } catch (error) {
     console.log(`Error: ${error}`);
   }
-
+  // Копируем файлы из папки files в папку files-copy
   try {
     const files = await fsPromises.readdir(pathToFolder);
     for (const file of files) {
